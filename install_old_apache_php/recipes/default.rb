0ed5f5688d23b9a8ec37b7cbbdc9a043c33819c3
trananbinh110 <an.tran@student.unsw.edu.au>
2014-01-23 13:46:24 +1100

    new way of install apache

--- install_old_apache_php/recipes/default.rb
+++ install_old_apache_php/recipes/default.rb
@@ -9,39 +9,32 @@
 
 # Only support Amazon Linux AMI
 
-# Modify Yum repo to an older repo
-template "/etc/yum.conf" do
-  source "yum.conf.erb"
-  owner "root"
+
+# Install Apache 2.2.2 and PHP 5.2.13
+cookbook_file "/tmp/httpd-2.2.2-1.x86_64.rpm" do
+  source "httpd-2.2.2-1.x86_64.rpm"
+end
+
+cookbook_file "/tmp/php-5.2.13-3rt.x86_64.rpm" do
+  source "php-5.2.13-3rt.x86_64.rpm"
+end
+
+execute "install_apache" do
+  cwd "/tmp"
+  user "root"
   group "root"
-  variables({
-     :release_ver => "2011.09"
-  })
+  command "yum localinstall httpd-2.2.2-1.x86_64.rpm"
 end
 
-# Install Apache 2.2.22 and PHP 5.3.10
-package "apache2" do
-  package_name "httpd"
-  action :install
-end
-
-package "php" do
-  package_name "php"
-  action :install
+execute "install_php" do
+  cwd "/tmp"
+  user "root"
+  group "root"
+  command "yum localinstall php-5.2.13-3rt.x86_64.rpm"
 end
 
 # Enable (at boot) and start apache2 service
 service "apache2" do
   service_name "httpd"
   action [:enable, :start]
-end
-
-# Reset Yum repo to latest repo
-template "/etc/yum.conf" do
-  source "yum.conf.erb"
-  owner "root"
-  group "root"
-  variables({
-     :release_ver => "latest"
-  })
 end
